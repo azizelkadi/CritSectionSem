984fd490fae25a430366e2f42298901842cdd9b9
Aziz <azizelkadi24@gmail.com>
2023-02-16 10:20:54 +0100

    add flush

--- semaphore_aziz.py
+++ semaphore_aziz.py
@@ -9,18 +9,18 @@ N = 8
 def task(semaphore, common, tid):
     a = 0
     for i in range(5):
-        print(f'{tid}-{i}: Non-critical Section')
+        print(f'{tid}-{i}: Non-critical Section', flush=True)
         a += 1
         time.sleep(random.random())
-        print(f'{tid}-{i}: End of non-critical Section')
+        print(f'{tid}-{i}: End of non-critical Section', flush=True)
 
         semaphore.acquire()
-        print(f'{tid}-{i}: Critical section')
+        print(f'{tid}-{i}: Critical section', flush=True)
         v = common.value + 1
-        print(f'{tid}-{i}: Inside critical section')
+        print(f'{tid}-{i}: Inside critical section', flush=True)
         time.sleep(random.random())
         common.value = v
-        print(f'{tid}-{i}: End of critical section')
+        print(f'{tid}-{i}: End of critical section', flush=True)
         semaphore.release()
 
 def main():
@@ -30,12 +30,12 @@ def main():
     common = Value('i', 0)
     for tid in range(N):
         lp.append(Process(target=task, args=(semaphore, common, tid)))
-    print (f"Valor inicial del contador {common.value}")
+    print (f"Valor inicial del contador {common.value}", flush=True)
     for p in lp:
         p.start()
     for p in lp:
         p.join()
-    print (f"Valor final del contador {common.value}")
+    print (f"Valor final del contador {common.value}", flush=True)
     print ("fin")
 
 if __name__ == "__main__":
